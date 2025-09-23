6806d49a0b66a692a3d40233a9c80c4791005800
Momin Shaikh <mominshaikh54849@gmail.com>
2025-09-23 16:14:27 +0000

    Create initial Hello World program

---
+++ PSP/Prog1.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // standard (input-output stream) input-out "h" header
+ // console (Direct console-control) input-out "h" header
+int main()
+{
+    printf("Hello\n");
+    //getch() // To output screen 
+    return 0;
+}
+
+// For executing plz use this cammand:- "gcc hello.c -o hello; .\hello.exe"
+
+
